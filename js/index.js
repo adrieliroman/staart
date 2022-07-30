6fb38f1154c8f12d5be69800288c9f8ccfe1cafb
adrieliroman <adrieliroman@gmail.com>
2022-07-30 16:08:29 +0100

    javaScript

--- js/index.js
+++ js/index.js
@@ -1,7 +1,36 @@
 
 function showAnswer(){
     var answ = document.querySelector("#answer")
-    console.log(answ)
-    answ.classList.toggle("blur")
-    
+    //* variavel nome da variavel = document.querySelector(aonde eu quero bbuscar as informacoes)("")(nome da tag, dentro de parentese e entre aspas, se for ID usar # se for classe usar .)
+    answ.classList.toggle("blur");
+    //Variavel contem todas as informacoes do html)//
+    //classList.toggle alterna uma funcao//
+    //blur ja foi definido no css//
+    //Adicionar ou remover funcao para a clase//
+    }
+function next(nextQuestion){
+        var question = document.querySelector("#card-container");
+        question.innerHTML="";
+        //modifica o que esta no html//
+        var cardDiv = document.createElement("div");
+        cardDiv.classList.add("card","animate__animated", "animate__backInRight");
+        cardDiv.innerHTML = `<div class="card">
+        <div class="card-title center">
+        <h1 class="right-title"> O que é ${nextQuestion.title}?</h1>
+        </div>
+            <div id="answer" class="card-description blur">
+                ${nextQuestion.description}
+            </div>
+    </div>`;
+    question.appendChild(cardDiv);
 }
+function search(){
+    fetch("https://flash.quickstaart.com/random")
+    .then(function(result){
+        return result.json()
+    })
+    .then(function(resultJson){
+        next(resultJson)
+    })
+}
+window.addEventListener("load", nextQuestion)
